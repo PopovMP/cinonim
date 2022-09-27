23e49e2684780587ed1ffbe2784b787ee4f625c1
Miroslav Popov <info@forexsb.com>
2022-09-27 17:25:19 +0300

    Fixed memory allocation.

--- test/watter/100.array.test.js
+++ test/watter/100.array.test.js
@@ -5,48 +5,43 @@ const {describe, it} = require('@popovmp/mocha-tiny')
 const {ciToWat}      = require('../../index')
 
 const src = `
-#export-func sum100 = sum100
+#export-func get13 = get13
 
-int numbers[100];
+double foo[100]; // ptr:    0, size: 8*100
+float  bar[100]; // ptr:  800, size: 4*100
+int    baz[100]; // ptr: 1200, size: 4*100
+long   gaz[100]; // ptr: 1600, size: 8*100
 
-int sum100()
+int get13()
 {
-	int i, sum;
+	int i, n;
+	
+	i = 13;
+	n = 42;
 
-	for (i=0; i<100; i+=1)
-		numbers[i] = i+1;
+	foo[i] = n; //    0 + 8*i
+	bar[i] = n; //  800 + 4*i
+	baz[i] = n; // 1200 + 4*i
+	gaz[i] = n; // 1600 + 8*i
 
-	sum=0;
-	for (i=0; i<100; i+=1)
-		sum += numbers[i];
-
-	return sum;
+	return baz[13];
 }
 `
 
 const expected = `
 (module
-    (export "sum100" (func $sum100))
+    (export "get13" (func $get13))
     (memory 1)
-    (func $sum100 (result i32)
+    (func $get13 (result i32)
         (local $i i32)
-        (local $sum i32)
-        (local.set $i (i32.const 0))
-        (block (loop
-            (br_if 1 (i32.eqz (local.get $i) (i32.const 100) (i32.lt_s)))
-            (i32.store (i32.add (i32.const 0) (i32.shl (local.get $i) (i32.const 2))) (local.get $i) (i32.const 1) (i32.add))
-            (local.set $i (local.get $i) (i32.const 1) (i32.add))
-            (br 0)
-        ))
-        (local.set $sum (i32.const 0))
-        (local.set $i (i32.const 0))
-        (block (loop
-            (br_if 1 (i32.eqz (local.get $i) (i32.const 100) (i32.lt_s)))
-            (local.set $sum (local.get $sum) (i32.load (i32.add (i32.const 0) (i32.shl (local.get $i) (i32.const 2)))) (i32.add))
-            (local.set $i (local.get $i) (i32.const 1) (i32.add))
-            (br 0)
-        ))
-        (local.get $sum)
+        (local $n i32)
+        (local.set $i (i32.const 13))
+        (local.set $n (i32.const 42))
+        (f64.store (i32.add (i32.const 0) (i32.shl (local.get $i) (i32.const 3))) (local.get $n))
+        (f32.store (i32.add (i32.const 800) (i32.shl (local.get $i) (i32.const 2))) (local.get $n))
+        (i32.store (i32.add (i32.const 1200) (i32.shl (local.get $i) (i32.const 2))) (local.get $n))
+        (i64.store (i32.add (i32.const 1600) (i32.shl (local.get $i) (i32.const 3))) (local.get $n))
+        (i32.load (i32.add (i32.const 1200) (i32.shl (i32.const 13) (i32.const 2))))
     )
 )
 `
